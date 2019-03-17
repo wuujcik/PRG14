29dbd72fa32199f77cfe6d73a792a0b03c606f8f
wuujcik <aleksandra.wojcikowska@tutanota.com>
2019-03-17 17:27:38 +0100

    Added map to FindFragment

--- app/src/main/java/com/wuujcik/prg14/FindFragment.java
+++ app/src/main/java/com/wuujcik/prg14/FindFragment.java
@@ -3,14 +3,29 @@ package com.wuujcik.prg14;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 
-public class FindFragment extends Fragment {
+public class FindFragment extends Fragment implements OnMapReadyCallback {
+
+    GoogleMap mGoogleMap;
+    MapView mMapView;
+    View rootView;
+
 
     public FindFragment() {
         // Required empty public constructor
@@ -20,11 +35,34 @@ public class FindFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View rootView = inflater.inflate(R.layout.fragment_find, container, false);
-
-
-
+        rootView = inflater.inflate(R.layout.fragment_find, container, false);
         return rootView;
     }
 
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        mMapView = (MapView) rootView.findViewById(R.id.map);
+        if (mMapView != null){
+            mMapView.onCreate(null);
+            mMapView.onResume();
+            mMapView.getMapAsync(this);
+
+        }
+
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+        mGoogleMap = googleMap;
+       // MapsInitializer.initialize(getContext());
+
+        LatLng prg14 = new LatLng(50.098420, 14.465882);
+        mGoogleMap.addMarker(new MarkerOptions().position(prg14).title("PRG14").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ORANGE)));
+        mGoogleMap.moveCamera(CameraUpdateFactory.newLatLng(prg14));
+        mGoogleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(prg14, 15.0f));
+    }
 }
